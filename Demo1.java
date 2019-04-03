cff7dbe75cc174aea1cee5acbab5a2f662c08b12
qq1021104560 <48001646+qq1021104560@users.noreply.github.com>
2019-04-03 19:15:46 +0800

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,16 @@
+       public class Demo1       
+  {
+   
+     public static void main(String[] arge)
+     {
+        char x = 'жа';
+	    int y = (int)x;
+         System.out.println((int)y);
+	        
+     }
+     public static int f(int abc)
+     {
+        int result = abc;
+          return result;
+     }
+   }
